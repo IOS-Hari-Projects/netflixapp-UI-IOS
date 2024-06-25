23f27c55536e7c41d715bdd6daf11da1f819a8ae
hari117 <hari.hari.cj@gmail.com>
2024-06-25 15:51:41 +0530

    Initial Commit

---
+++ netflixapp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  netflixapp
+//
+//  Created by Hari Krishna S on 25/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ netflixapp/netflixappApp.swift
@@ -0,0 +1,17 @@
+//
+//  netflixappApp.swift
+//  netflixapp
+//
+//  Created by Hari Krishna S on 25/06/24.
+//
+
+import SwiftUI
+
+@main
+struct netflixappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
